1cab69ba4c62371e73d7985be57d722429ddb926
Alonso <josealonsomendoza@hotmail.com>
2019-07-12 08:51:25 -0500

    Added options for video

--- client.py
+++ client.py
@@ -18,6 +18,11 @@ URL = 'http://10.0.0.5:8082'
 
 loop = asyncio.get_event_loop()
 
+options = {
+    "framerate": "30",
+    "video_size": "640x480"
+}
+
 
 # Class representing each peer connection
 class PeerConnection:
@@ -26,7 +31,7 @@ class PeerConnection:
         self.remote_id = remote_id
         self.pc = RTCPeerConnection()
         # Test if video file exists, if not, then set default green frames
-        self.pc.addTrack(MediaPlayer('/dev/video0', options={'video_size': '640x480'}).video)
+        self.pc.addTrack(MediaPlayer('/dev/video0', options=options).video)
         # if os.path.isfile(os.path.join(ROOT, 'robot.mp4')):
         #     self.pc.addTrack(MediaPlayer(os.path.join(ROOT, 'robot.mp4')).video)
         # else:
