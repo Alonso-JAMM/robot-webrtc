2e645029a2e2f36e0fa7a15722923e52a177ba29
Alonso <jalonsomendoza32@gmail.com>
2020-05-01 19:31:24 -0500

    Fixed description of project

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="robot_client",
     version="0.1",
-    description="robot client for easyrtc ",
+    description="robot client for Janus ",
     packages=find_packages(),
 )
